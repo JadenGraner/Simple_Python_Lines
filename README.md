14f3036430f395ec27882aa71fd6c486c1d00f78
Jaden <drakeredcrest12@hotmail.com>
2019-05-30 10:55:46 -0400

    now generates an image

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 Just a simple line drawing program I came up with the other day.
 	My goal was to see what I could make with no outside knowledge on computer
 graphics so my apologies if I'm using poor practices or incorrect terminology.
-All you should need to run the program is python(3.5.2+) and numpy
+All you should need to run the program is python(3.5.2+), numpy, and pillow
+
+This version: 5/30/19
+-Moved the comments because they were bothering me when I was just trying to read the code
+-Added pillow so I could output an image of the lines drawn instead of a printed array
+-added os so I could put the image above into the folder containing the program file
+-added my name on the top for that clout
+
+Jaden Graner 2019
--- Vector.py
+++ Vector.py
@@ -1,41 +1,45 @@
+import os
 import numpy as np
 import math
+from PIL import Image
+# Jaden M Graner 2019
 
-# Widdth and height of display in pixels; and the formation of the display (output)
-x,y = 10,10
-Screen = np.zeros((x,y),dtype=int)
-# Aspect Ratio of pixels (As in if the pixels are rectangular or you wish to manipulate the image)
-Pixelx,Pixely = 1,1
-# The vertical and horizontal distance to the border between pixels, used for detecting wither an object falls within the area
-Pixelxrad,Pixelyrad = Pixelx/2,Pixely/2
-# The number of locations on an edge that are checked for visability
-Line_Cuts = 10
 
-# The array containing the Verts
-Dots = np.array([[0,0],[8,2],[5,8]],dtype=np.float32)
-# Array containing the edges (connections between verts)
-Lines = np.array([[0,1],[1,2]],dtype=np.int)
-# Empty Array for selected segments(Verts Along Edges) to be drawn, Should be a 2D array but I'm stupid, See FIX_1
-Render = []
-# Get each edge in the image and break it up into pixel checking locations and put it in Render
-for A,B in Lines:
+x,y = 10,10 # Widdth and height of display in pixels
+Screen = np.zeros((x,y,3),dtype=np.uint8) # The formation of the display (output), As well as 3Z layrs for future RGB support
+
+Pixelx,Pixely = 1,1 # Aspect Ratio of pixels (As in if the pixels are rectangular or you wish to manipulate the image)
+Pixelxrad,Pixelyrad = Pixelx/2,Pixely/2 # The vertical and horizontal distance to the border between pixels, used for detecting wither an object falls within the area
+Line_Cuts = 10 # The number of locations on an edge that are checked for visability
+
+Dots = np.array([[0,0],[8,2],[5,8]],dtype=np.int) # The array containing the Verts
+
+Lines = np.array([[0,1],[1,2]],dtype=np.int) # Array containing the edges (connections between verts)
+
+Render = [] # Empty Array for selected segments(Verts Along Edges) to be drawn, Should be a 2D array but I'm stupid, See FIX_1
+
+for A,B in Lines: # Get each edge in the image and break it up into pixel checking locations and put it in Render
 	Length = (Dots[B] - Dots[A])/Line_Cuts
 	for Seg in range(Line_Cuts+1):
 		Next = (Dots[A] + (Length*Seg))
 		Render = np.append(Render,Next,axis=0)
 
-# Convert the list render into an array, see FIX_1, It shoudl already be a 2D array here
-Render = np.asarray(Render)
-# Needed for a for loop later, see FIX_1
-Renlen = len(Render)//2
-# Check Each Pixel and see if any of the points in Render fall within them
-for i in range(x):
+Render = np.asarray(Render) # Convert the list render into an array, see FIX_1, It shoudl already be a 2D array here
+Renlen = len(Render)//2 # Needed for a for loop later, see FIX_1
+
+for i in range(x): # Check Each Pixel and see if any of the points in Render fall within them
 	for j in range(y):
 		for k in range(Renlen): # These Three lines used to be a nice clean "for Rendx,Rendy in Render" but I can't, see FIX_1
 			Rendy = Render[k*2] # FIX_1
 			Rendx = Render[(k*2)+1] # FIX_1
 			if (Rendx > i-Pixelxrad) and (Rendx <= i+Pixelxrad):
 				if (Rendy > j-Pixelyrad) and (Rendy <= j+Pixelyrad):
-					Screen[i,j] = 1
-# Print result to Console
-print(Screen)
+					Screen[i,j] = [255,255,255] #RGB
+
+# print(Screen) # Print result to Console #REMOVED 5/30/19 REPLACED with image display
+
+local = os.path.dirname(os.path.realpath(__file__)) # location for new file, here it'll be in the same directory as the py file
+
+img = Image.fromarray(Screen,mode='RGB') # Simply convert the output array to an image
+img.save(local+'/'+'output.tiff') # Location to put the new image # It Didn't like L+'/my.tiff', so I had to use local+'/'+'my.tiff'
+img.show() # Show image when done
